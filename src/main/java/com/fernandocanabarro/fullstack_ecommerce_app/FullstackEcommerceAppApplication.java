d4e65afe03c383e1a248dcb0bed39f24caf941b2
FernandoCanabarroAhnert <ahnertfernando499@gmail.com>
2025-02-05 17:07:13 -0300

    :tada: init: Projeto Criado

---
+++ src/main/java/com/fernandocanabarro/fullstack_ecommerce_app/FullstackEcommerceAppApplication.java
@@ -0,0 +1,13 @@
+package com.fernandocanabarro.fullstack_ecommerce_app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FullstackEcommerceAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FullstackEcommerceAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/fernandocanabarro/fullstack_ecommerce_app/FullstackEcommerceAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fernandocanabarro.fullstack_ecommerce_app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FullstackEcommerceAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
